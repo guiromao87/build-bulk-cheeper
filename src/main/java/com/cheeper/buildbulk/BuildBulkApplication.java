526e92f1ddcde57c20f356ee4050a78f8ab99228
Guilherme Romão <guilhermersimoes87@gmail.com>
2020-08-09 20:07:21 -0300

    initial commit

---
+++ src/main/java/com/cheeper/buildbulk/BuildBulkApplication.java
@@ -0,0 +1,13 @@
+package com.cheeper.buildbulk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildBulkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildBulkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheeper/buildbulk/BuildBulkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheeper.buildbulk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildBulkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
